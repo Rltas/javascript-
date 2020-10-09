048b638441026b7a8ac119b81e47094029ba4dfd
Rltas <3520297341@qq.com>
2020-10-09 10:37:11 +0800

    时间间隔更新

--- default.js
+++ default.js
@@ -301,53 +301,48 @@ class CommonTool {
    *时间戳间隔
    */
 
-  dateDiff(timestamp) {
-    let arrTimestamp = (timestamp + '').split('');
-    for (let start = 0; start < 13; start++) {
-      if (!arrTimestamp[start]) {
-        arrTimestamp[start] = '0';
-      }
-    }
-    timestamp = arrTimestamp.join('') * 1;
-    let minute = 1000 * 60;
+  dateDiff(dateTimeStamp) {
+    let minute = 1000 * 60; //把分，时，天，周，半个月，一个月用毫秒表示
     let hour = minute * 60;
     let day = hour * 24;
+    let week = day * 7;
     let halfamonth = day * 15;
     let month = day * 30;
-    let now = new Date().getTime();
-    let diffValue = now - timestamp;
+    let now = new Date().getTime(); //获取当前时间毫秒
+    let diffValue = now - dateTimeStamp; //时间差
     if (diffValue < 0) {
-      return '不久前';
+      return;
     }
-    let monthC = diffValue / month;
-    let weekC = diffValue / (7 * day);
-    let dayC = diffValue / day;
+    let minC = diffValue / minute; //计算时间差的分，时，天，周，月
     let hourC = diffValue / hour;
-    let minC = diffValue / minute;
-    let zero = function (value) {
-      if (value < 10) {
-        return '0' + value;
-      }
-      return value;
-    };
-    if (monthC > 12) {
-      return (function () {
-        let date = new Date(timestamp);
-        return date.getFullYear() + '年' + zero(date.getMonth() + 1) + '月' +
-          zero(date.getDate()) + '日';
-      })();
-    } else if (monthC >= 1) {
-      return parseInt(monthC) + "月前";
-    } else if (weekC >= 1) {
-      return parseInt(weekC) + "周前";
-    } else if (dayC >= 1) {
-      return parseInt(dayC) + "天前";
-    } else if (hourC >= 1) {
-      return parseInt(hourC) + "小时前";
-    } else if (minC >= 1) {
-      return parseInt(minC) + "分钟前";
+    let dayC = diffValue / day;
+    let weekC = diffValue / week;
+    let monthC = diffValue / month;
+    let result;
+    if (monthC >= 1 && monthC <= 3) {
+      result = " " + parseInt(monthC) + "月前"
+    } else if (weekC >= 1 && weekC <= 3) {
+      result = " " + parseInt(weekC) + "周前"
+    } else if (dayC >= 1 && dayC <= 6) {
+      result = " " + parseInt(dayC) + "天前"
+    } else if (hourC >= 1 && hourC <= 23) {
+      result = " " + parseInt(hourC) + "小时前"
+    } else if (minC >= 1 && minC <= 59) {
+      result = " " + parseInt(minC) + "分钟前"
+    } else if (diffValue >= 0 && diffValue <= minute) {
+      result = "刚刚"
+    } else {
+      let datetime = new Date();
+      datetime.setTime(dateTimeStamp);
+      let Nyear = datetime.getFullYear();
+      let Nmonth = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+      let Ndate = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+      let Nhour = datetime.getHours() < 10 ? "0" + datetime.getHours() : datetime.getHours();
+      let Nminute = datetime.getMinutes() < 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+      let Nsecond = datetime.getSeconds() < 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
+      result = Nyear + "-" + Nmonth + "-" + Ndate
     }
-    return '刚刚';
+    return result;
   }
 
 }
